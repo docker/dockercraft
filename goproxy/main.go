dc6c604f3bcbc2bec86ee8ff5ec1481ee82f171d
Adrien Duermael <adrien@docker.com>
2015-06-23 16:57:02 -0700

    Init dockerClient only one time

--- goproxy/main.go
+++ goproxy/main.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var DOCKER_CLIENT *dockerclient.DockerClient
+
 // Callback used to listen to Docker's events
 func eventCallback(event *dockerclient.Event, ec chan error, args ...interface{}) {
 
@@ -22,44 +24,51 @@ func eventCallback(event *dockerclient.Event, ec chan error, args ...interface{}
 
 	switch event.Status {
 	case "create":
-		//fmt.Println("create event")
+		fmt.Println("create event")
 	case "start":
+
+		repo, tag := splitRepoAndTag(event.From)
+
+		containerName := "<name>"
+
+		containerInfo, err := DOCKER_CLIENT.InspectContainer(id)
+
+		if err != nil {
+			fmt.Print("InspectContainer error:", err.Error())
+		} else {
+			containerName = containerInfo.Name
+		}
 
 		data := url.Values{
 			"action":    {"startContainer"},
 			"id":        {id},
-			"name":      {"<name>"},
-			"imageRepo": {"<imageName>"},
-			"imageTag":  {"<imageTag>"}}
+			"name":      {containerName},
+			"imageRepo": {repo},
+			"imageTag":  {tag}}
 
 		MCServerRequest(data, client)
 
 	case "stop":
-		//fmt.Println("")
+		fmt.Println("stop event")
 	case "restart":
-		//fmt.Println("")
+		fmt.Println("restart event")
 	case "kill":
-		//fmt.Println("")
+		fmt.Println("kill event")
 	case "die":
-		//fmt.Println("")
+		fmt.Println("die event")
 	}
 }
 
 func listContainers(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("listContainers(1)")
-
-	docker, _ := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
-	containers, err := docker.ListContainers(false, false, "")
+	containers, err := DOCKER_CLIENT.ListContainers(false, false, "")
 
 	if err != nil {
 		io.WriteString(w, err.Error())
 		return
 	}
 
-	fmt.Println("listContainers(2)")
-
-	images, err := docker.ListImages()
+	images, err := DOCKER_CLIENT.ListImages()
 
 	if err != nil {
 		io.WriteString(w, err.Error())
@@ -71,7 +80,7 @@ func listContainers(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < len(containers); i++ {
 
 		id := containers[i].Id
-		info, _ := docker.InspectContainer(id)
+		info, _ := DOCKER_CLIENT.InspectContainer(id)
 		name := info.Name[1:]
 		//image := strings.Split(info.Image, ":")
 		imageRepo := ""
@@ -80,21 +89,11 @@ func listContainers(w http.ResponseWriter, r *http.Request) {
 		for _, image := range images {
 			if image.Id == info.Image {
 				if len(image.RepoTags) > 0 {
-					repoAndTag := strings.Split(image.RepoTags[0], ":")
-
-					if len(repoAndTag) > 0 {
-						imageRepo = repoAndTag[0]
-					}
-
-					if len(repoAndTag) > 1 {
-						imageTag = repoAndTag[1]
-					}
+					imageRepo, imageTag = splitRepoAndTag(image.RepoTags[0])
 				}
 				break
 			}
 		}
-
-		fmt.Println("listContainers(3)")
 
 		data := url.Values{
 			"action": {"startContainer"},
@@ -106,8 +105,6 @@ func listContainers(w http.ResponseWriter, r *http.Request) {
 			"status":    {strconv.FormatBool(info.State.Running)},
 		}
 
-		fmt.Println("listContainers(4)")
-
 		MCServerRequest(data, client)
 	}
 
@@ -116,35 +113,49 @@ func listContainers(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/containers", listContainers)
-	http.ListenAndServe(":8000", nil)
-
 	// Init the client
-	docker, _ := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+	DOCKER_CLIENT, _ = dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
 
 	// Monitor events
-	docker.StartMonitorEvents(eventCallback, nil)
+	DOCKER_CLIENT.StartMonitorEvents(eventCallback, nil)
+
+	go func() {
+		http.HandleFunc("/containers", listContainers)
+		http.ListenAndServe(":8000", nil)
+	}()
 
 	// wait for interruption
 	<-make(chan int)
+}
+
+func splitRepoAndTag(repoTag string) (string, string) {
+
+	repo := ""
+	tag := ""
+
+	repoAndTag := strings.Split(repoTag, ":")
+
+	if len(repoAndTag) > 0 {
+		repo = repoAndTag[0]
+	}
+
+	if len(repoAndTag) > 1 {
+		tag = repoAndTag[1]
+	}
+
+	return repo, tag
 }
 
 // MCServerRequest send a POST request that will be handled
 // by our MCServer Docker plugin.
 func MCServerRequest(data url.Values, client *http.Client) {
 
-	fmt.Println("MCServerRequest (1)")
-
 	if client == nil {
 		client = &http.Client{}
 	}
-
-	fmt.Println("MCServerRequest (2)")
 
 	req, _ := http.NewRequest("POST", "http://127.0.0.1:8080/webadmin/Docker/Docker", strings.NewReader(data.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.SetBasicAuth("admin", "admin")
 	client.Do(req)
-
-	fmt.Println("MCServerRequest (3)")
 }
