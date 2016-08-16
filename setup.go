001a38a92b82b61646cf78222496f9c99876e2a4
Gaetan de Villele <gdevillele@gmail.com>
2016-08-16 15:33:16 -0700

    report changes from PR #65 and clean them a bit
    Signed-off-by: Gaetan de Villele <gdevillele@gmail.com>
    (cherry picked from commit 4e81969a3e37b63ce6b84a0f0d9acca913045094)

--- setup.go
+++ setup.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"io"
 	"net/http"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -28,15 +32,23 @@ func (d *Daemon) GetDockerBinary() error {
 			return err
 		}
 		defer out.Close()
-		resp, err := http.Get("https://get.docker.com/builds/Linux/x86_64/" + d.BinaryName)
+
+		// the method of downloading it is different for version >= 1.11.0
+		// (in which case it is an archive containing multiple binaries)
+		versionComp, err := compareVersions(d.Version, "1.11.0")
 		if err != nil {
 			return err
 		}
-		defer resp.Body.Close()
-
-		_, err = io.Copy(out, resp.Body)
-		if err != nil {
-			return err
+		if versionComp >= 0 {
+			err = getClient(out, "https://get.docker.com/builds/Linux/x86_64/docker-"+d.Version+".tgz", extractClient)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = getClient(out, "https://get.docker.com/builds/Linux/x86_64/docker-"+d.Version, copyClient)
+			if err != nil {
+				return err
+			}
 		}
 
 		err = os.Chmod(filename, 0700)
@@ -45,6 +57,84 @@ func (d *Daemon) GetDockerBinary() error {
 		}
 	} else {
 		log.Infof("docker binary (version %s) found!", d.Version)
+	}
+	return nil
+}
+
+// Utility functions
+
+type copier func(out *os.File, resp *http.Response) error
+
+func compareVersions(v1 string, v2 string) (comp int, err error) {
+	v1Parts := strings.Split(v1, ".")
+	v2Parts := strings.Split(v2, ".")
+
+	for i := 0; i < len(v1Parts) && i < len(v2Parts); i++ {
+		v1int, err := strconv.Atoi(v1Parts[i])
+		if err != nil {
+			return -2, err
+		}
+
+		v2int, err := strconv.Atoi(v2Parts[i])
+		if err != nil {
+			return -2, err
+		}
+
+		if v1int < v2int {
+			return -1, nil
+		} else if v1int > v2int {
+			return 1, nil
+		}
+	}
+
+	if len(v1Parts) < len(v2Parts) {
+		return -1, nil
+	} else if len(v1Parts) > len(v2Parts) {
+		return 1, nil
+	}
+	return 0, nil
+}
+
+func getClient(out *os.File, URL string, cp copier) error {
+	resp, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	err = cp(out, resp)
+	return err
+}
+
+func copyClient(out *os.File, resp *http.Response) error {
+	_, err := io.Copy(out, resp.Body)
+	return err
+}
+
+func extractClient(out *os.File, resp *http.Response) error {
+	gr, err := gzip.NewReader(resp.Body)
+	defer gr.Close()
+	if err != nil {
+		return err
+	}
+
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if hdr.Typeflag == tar.TypeReg && hdr.Name == "docker/docker" {
+			_, err = io.Copy(out, tr)
+			if err != nil {
+				return err
+			}
+			break
+		}
+		// logrus.Println("not yet")
 	}
 	return nil
 }
